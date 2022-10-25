809eb5ba84679fb69121d68e27cff50e704a8a8c
arielpedroso95 <94135168+arielpedroso95@users.noreply.github.com>
2022-10-25 20:15:32 -0300

    criação do app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+/*Aluno 1- Responsável por criar o projeto no git e dar acesso aos outros (Ariel)
+
+Aluno 2 - Responsável por criar a função de listar todos os usuários acima de 18 anos. (Guilherme) 
+
+Aluno 3 - Responsável por criar a função de listar todos os usuários inserindo mais uma informação para cada de ativo: true
+
+Aluno 4 - Responsável por criar a função de adicionar um usuário- opcional
+
+Aluno 5- Responsável por criar a função de remover um usuário-opcional*/
+
+const usuarios = require('./users.json')
+
